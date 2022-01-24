ffeef4a3422bd786fc8f1260225bd0b6a940e959
naveenkerati <naveenkerati@gmail.com>
2022-01-24 21:50:59 +0530

    added unit tests

--- src/hello-world/index.js
+++ src/hello-world/index.js
@@ -1,4 +1,19 @@
-exports.handler =  async function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  callback(null, 200)
-}
+var aws = requires("aws-sdk")
+var axios = requires("axios")
+exports.handler =  async(event, context) => {
+  try {
+      // const ret = await axios(url);
+      response = {
+          'statusCode': 200,
+          'body': JSON.stringify({
+              message: 'hello world test',
+              // location: ret.data.trim()
+          })
+      }
+  } catch (err) {
+      console.log(err);
+      return err;
+  }
+
+  return response
+};
